f10cadcfd39ead55e70a5f763cf3bebe59944b94
Shivain Saxena <shivainsaxena@gmail.com>
2024-02-26 23:14:04 -0700

    Made animation responsive for 1200px now need to work on text portion and mobile responsiveness

---
+++ src/animations/Skillstar.js
@@ -0,0 +1,31 @@
+import './Star.css';
+import React from "../assets/reactelectron.webp";
+import Python from "../assets/pythonelectron.png";
+import Javascript from '../assets/javascriptelectron1.png';
+import CSS from '../assets/csselectron.png';
+import HTML from '../assets/htmlelectron.png';
+
+const Skillstar = () => {
+    return ( 
+        <div id="atom">
+            <div id="nucleus"></div>
+            <div class="orbit">
+                <div class="electron"><img src={React} alt="React Electron" className='react'/></div>
+            </div>
+            <div class="orbit">
+                <div class="electron"><img src={Python} alt="Python Electron" className='python'/></div>
+            </div>
+            <div class="orbit">
+                <div class="electron"><img src={Javascript} alt="Javascript Electron" className='javascript'/></div>
+            </div>
+            <div class="orbit">
+                <div class="electron"><img src={CSS} alt="CSS Electron" className='css'/></div>
+            </div>
+            <div class="orbit">
+                <div class="electron"><img src={HTML} alt="HTML Electron" className='html'/></div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Skillstar;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,35 @@
 import Footer from "./Footer";
+import { Grid, Typography, Box } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
+import '../App.css';
+import Skillstar from "../animations/Skillstar";
 
 
 const Skills = () => {
     return ( 
-        <div>
-            <h1>akdjflakjflaskf</h1>
-        </div>
-        
+        <StyledEngineProvider injectFirst>
+            <Box display={"flex"} justifyContent={"center"}>
+               <Typography className="skillsHeader">
+                    My Skills 
+                </Typography> 
+            </Box>
+            <Grid container spacing={1}>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={11}>
+                    <Box display={"flex"} flexDirection={{xs: 'column-reverse', lg: 'row'}} alignItems={{xs: "center", lg: 'flex-start '}}>
+                        <Box flexGrow={1}>
+                            <Typography>
+                                Languages
+                            </Typography>
+                        </Box>
+                        <Box flexGrow={1} display={'flex'} justifyContent={"end"} alignItems={"end"} marginRight={10}>
+                            <Skillstar />
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+            
+        </StyledEngineProvider>
      );
 }
  
