93c33daf8794b5cb8dc2bca672e86bd0aa0eed98
Shivain Saxena <shivainsaxena@gmail.com>
2024-02-28 00:10:38 -0700

    Finished skill section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import Footer from "./Footer";
-import { Grid, Typography, Box } from '@mui/material';
+import { Grid, Typography, Box, Chip } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
 import '../App.css';
 import Skillstar from "../animations/Skillstar";
@@ -13,21 +13,55 @@ const Skills = () => {
                     My Skills 
                 </Typography> 
             </Box>
-            <Grid container spacing={1}>
-                <Grid item xs={1}></Grid>
-                <Grid item xs={11}>
+            <Grid container spacing={1} mb={'clamp(6.25rem, 5rem + 6.25vw, 12.5rem)'}>
+                <Grid item xs={12}>
                     <Box display={"flex"} flexDirection={{xs: 'column-reverse', lg: 'row'}} alignItems={{xs: "center", lg: 'flex-start '}}>
-                        <Box flexGrow={1}>
-                            <Typography>
+                        <Box display={"flex"} flexDirection={'column'} flexGrow={1} marginLeft={{xs: 0, lg: 'clamp(3.125rem, -5.208rem + 11.111vw, 8.125rem)'}} my={10} alignItems={{xs: 'center', lg: 'flex-start'}}>
+                            <Typography className="languageTitle" >
                                 Languages
                             </Typography>
+                            <Box display={"flex"} gap={4} flexWrap={'wrap'} padding={5} justifyContent={{xs: 'center', lg: 'flex-start'}}>
+                                <Chip
+                                label="Python"
+                                color="info"/>
+                                <Chip
+                                label="JavaScript"
+                                color="info"/>
+                                <Chip
+                                label="HTML"
+                                color="info"/>
+                                <Chip
+                                label="CSS"
+                                color="info"/>
+                                <Chip
+                                label="C++"
+                                color="info"/>
+                            </Box>
+                            <Typography className="languageTitle" mt={'clamp(1.563rem, 0.875rem + 3.438vw, 5rem)'}>
+                                Frameworks / Tools
+                            </Typography>
+                            <Box display={"flex"} gap={4} padding={5} flexWrap={'wrap'} justifyContent={{xs: 'center', lg: 'flex-start'}}>
+                                <Chip
+                                label="React.js"
+                                color="info"/>
+                                <Chip
+                                label="Git"
+                                color="info"/>
+                                <Chip
+                                label="GitHub"
+                                color="info"/>
+                                <Chip
+                                label="Material UI"
+                                color="info"/>
+                            </Box>
                         </Box>
-                        <Box flexGrow={1} display={'flex'} justifyContent={"end"} alignItems={"end"} marginRight={10}>
+                        <Box flexGrow={1} display={'flex'} justifyContent={"end"} alignItems={"end"} marginRight={{xs: 0, lg: 'clamp(3.125rem, -5.208rem + 11.111vw, 8.125rem)'}}>
                             <Skillstar />
                         </Box>
                     </Box>
                 </Grid>
             </Grid>
+            <Footer/>
             
         </StyledEngineProvider>
      );
